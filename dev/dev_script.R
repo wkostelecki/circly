7ca75280bf8e2673b1603e7dc22969a070be9cbd
Wojtek Kostelecki <wkostelecki@gmail.com>
2014-09-27 22:08:45 +0100

    Tweaks.

--- dev/dev_script.R
+++ dev/dev_script.R
@@ -3,9 +3,9 @@ library(circly)
 library(circlize)
 # source('./circly.R')
 
-NCOL = 2
-NROW = 2
-# set.seed(1)
+NCOL = 4
+NROW = 4
+set.seed(5)
 M1 = matrix(runif(NCOL * NROW), ncol = NCOL)
 M2 = matrix(runif(NCOL * NROW), ncol = NCOL)
 
@@ -15,16 +15,23 @@ M2 = matrix(runif(NCOL * NROW), ncol = NCOL)
 M1 = matrix(sample((1:(NCOL * NROW)) ^ 2), ncol = NCOL)
 M2 = matrix(sample(((NCOL * NROW):1) ^ 2), ncol = NCOL)
 
-colnames = LETTERS[1:NCOL]
+# colnames = sapply(1:NCOL, function(x) paste0(sample(LETTERS, 8), collapse = ''))
+# colnames = LETTERS[1:NCOL]
 # colnames[2] = 'B\nBC'
-rownames = letters[1:NROW]
+# rownames = letters[1:NROW]
+# rownames[2] = 'safasdf\nsadfsadg'
 
-colnames(M1) = colnames
-colnames(M2) = colnames
-rownames(M1) = rownames
-rownames(M2) = rownames
+# colnames(M1) = colnames
+# colnames(M2) = colnames
+# rownames(M1) = rownames
+# rownames(M2) = rownames
 
 circly(M1, M2)
+
+# png('./tmp.png', height = 1000, width = 1000)
+# circly(M1, M2, color_column = rgb(c(1,0,0,0.5), c(0,1,0,0), c(0,0,1,0.5)),
+#        color_row = rgb(c(0.5,0,0.2,0.8), c(0.5,0.5,0.8,0.2), c(0,0.5,0.2,0.2)))
+# dev.off()
 
 #chordDiagram(t(M1)[ncol(M1):1, , drop = F], transparency = 0.5)
 
