538c8b25b90d3a295499a26b1f20d60316b69421
Wojtek Kostelecki <wkostelecki@gmail.com>
2014-12-08 21:55:51 +0000

    incorporated outer_segment function.

--- R/chord_shapes.R
+++ R/chord_shapes.R
@@ -53,37 +53,53 @@ chord_shapes = function(data, from_amount, to_amount, from_label, to_label){
   in_end = -(in_end * compress + gaps - 0.5) * (ROI / (ROI + 1)) + 0.25
   # bottoms segments ----
   
-  r1 = 1.03
-  r2 = 1.1
+#   r1 = 1.03
+#   r2 = 1.1
   
-  outer_segments = data.frame()
+  inner_r = 1.03
+  outer_r = 1.1
   
-  for (i in 1:length(out_start)) {
-    theta <- 2 * pi * seq(out_start[i], out_end[i], length = 100)
-    
-    x = c(r1 * cos(theta), rev(r2 * cos(theta)))
-    y = c(r1 * sin(theta), rev(r2 * sin(theta)))
-    
-    outer_segments = rbind(outer_segments,
-                           data.frame(ID = paste('Out', i),
-                                      x = x,
-                                      y = y))
-  }
+#   outer_segments = data.frame()
+#   
+#   for (i in 1:length(out_start)) {
+#     theta <- 2 * pi * seq(out_start[i], out_end[i], length = 100)
+#     
+#     x = c(r1 * cos(theta), rev(r2 * cos(theta)))
+#     y = c(r1 * sin(theta), rev(r2 * sin(theta)))
+#     
+#     outer_segments = rbind(outer_segments,
+#                            data.frame(ID = paste('Out', i),
+#                                       x = x,
+#                                       y = y))
+#   }
+#   
+#   # top segments ----
+#   for (i in 1:length(in_start)) {
+#     theta <- 2 * pi * seq(in_start[i], in_end[i], length = 100)
+#     
+#     x = c(r1 * cos(theta), rev(r2 * cos(theta)))
+#     y = c(r1 * sin(theta), rev(r2 * sin(theta)))
+#     
+#     outer_segments = rbind(outer_segments,
+#                            data.frame(ID = paste('In', i),
+#                                       x = x,
+#                                       y = y))
+#     
+#   }
   
-  # top segments ----
-  for (i in 1:length(in_start)) {
-    theta <- 2 * pi * seq(in_start[i], in_end[i], length = 100)
-    
-    x = c(r1 * cos(theta), rev(r2 * cos(theta)))
-    y = c(r1 * sin(theta), rev(r2 * sin(theta)))
-    
-    outer_segments = rbind(outer_segments,
-                           data.frame(ID = paste('In', i),
-                                      x = x,
-                                      y = y))
-    
-  }
+  start = c(out_start, in_start)
+  end = c(out_end, in_end)
   
+  IDS = c(paste('OUT', 1:length(out_start)),
+          paste('IN', 1:length(in_start)))
+  
+  
+  outer_segments = lapply(1:length(start),
+                          function(i){
+                            circly:::outer_segment(start[i], end[i], inner_r, outer_r) %>%
+                              mutate(ID = IDS[i])
+                          }) %>% do.call(rbind, .) %>%
+    mutate(ID = factor(ID, IDS))
   
   return(list(outer_segments = outer_segments,
               chords = data.frame()))
