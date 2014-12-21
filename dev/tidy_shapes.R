2efd0d764ef1f0d2bbb546e087949183a78a2f24
Wojtek Kostelecki <wkostelecki@gmail.com>
2014-12-21 22:19:44 +0000

    viewing chord segments

--- dev/tidy_shapes.R
+++ dev/tidy_shapes.R
@@ -2,8 +2,8 @@ library(circly)
 library(ggplot2)
 remove(list = ls())
 
-NROW = 2
-NCOL = 25
+NROW = 5
+NCOL = 5
 
 #----
 data = data.frame(FROM_AMOUNT = 1:(NROW * NCOL),
@@ -19,6 +19,12 @@ to_amount = 'TO_AMOUNT'
 shapes = chord_shapes(data, from_amount, to_amount, from_label, to_label)
 
 #----
+chords = circly:::chord_segment(0.6, 0.7, 0.2, 0.4)
+ggplot(chords) + geom_polygon(aes(x, y)) + coord_equal() + ylim(1.1 * c(-1, 1)) + xlim(1.1 * c(-1, 1))
+
+
+#----
+
 g = ggplot() +
   geom_polygon(data = shapes$outer_segments,
                aes(x, y, group = ID, fill = ID)) +
