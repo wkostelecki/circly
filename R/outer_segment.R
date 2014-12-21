af2dd33d86b4e7d28693ebe44d47aac72588113f
Wojtek Kostelecki <wkostelecki@gmail.com>
2014-12-21 10:44:05 +0000

    outer_segment documentation.

--- R/outer_segment.R
+++ R/outer_segment.R
@@ -1,4 +1,11 @@
-#' creates x,y boundaries for outer segments of a circle
+#' @title outer_segment
+#' @description Creates x,y boundaries for outer segments of the chord diagram 
+#'   circle. \cr start and end arguments are in the interval [0, 1] and are
+#'   mapped to [0, 2 * pi]
+#' @param start point on circle (value in [0, 1])
+#' @param end point on circle (value in [0, 1])
+#' @param inner_r inner radius of segment
+#' @param outer_r outer radius of segment
 outer_segment = function(start, end, inner_r, outer_r){
   
   theta <- 2 * pi * seq(start, end, length = 100)
