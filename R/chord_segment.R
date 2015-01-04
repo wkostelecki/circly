5d064a86815f46e532c5e9edac35119041a6f604
Wojtek Kostelecki <wkostelecki@gmail.com>
2015-01-04 23:38:06 +0000

    Function for chord shapes.

--- R/chord_segment.R
+++ R/chord_segment.R
@@ -33,3 +33,59 @@ chord_segment = function(out_start, out_end, in_start, in_end){
              y = y)
   
 }
+
+#' @import dplyr
+chord_segments = function(data, segment_spaces = NULL){
+  
+  if (is.null(segment_spaces)){
+    segment_spaces = outer_spacing(data)
+  }
+  
+  out = data.frame(FROM = data$FROM,
+                   TO = data$TO,
+                   OUT_START = NA,
+                   OUT_END = NA,
+                   IN_START = NA,
+                   IN_END = NA)
+  
+  for (i in 1:nrow(segment_spaces)){
+    
+    this_segment = segment_spaces[i, ]
+    
+    
+    
+    if (this_segment$Direction == 'In'){
+      ind = which(data$TO == as.character(this_segment$Label))
+      this_seq = subset(data, TO == as.character(this_segment$Label),
+                        select = 'IN', drop = TRUE)
+    } else if(this_segment$Direction == 'Out'){
+      ind = which(data$FROM == as.character(this_segment$Label))
+      this_seq = subset(data, FROM == as.character(this_segment$Label),
+                        select = 'OUT', drop = TRUE)
+    }
+    
+    size = with(this_segment, End - Start)
+    n_seq = c(0, cumsum(this_seq) / sum(this_seq)) * size + this_segment$Start
+    
+    if (this_segment$Direction == 'In'){
+      out$IN_START[ind] = n_seq[1:(length(n_seq) - 1)]
+      out$IN_END[ind] = n_seq[2:length(n_seq)]
+    } else if(this_segment$Direction == 'Out'){
+      out$OUT_END[ind] = n_seq[1:(length(n_seq) - 1)]
+      out$OUT_START[ind] = n_seq[2:length(n_seq)]
+    }
+    
+  }
+  
+  #----
+  OUT = vector('list', nrow(out))
+  for (i in 1:nrow(data)){
+    OUT[[i]] = with(out, chord_segment(OUT_START[i], OUT_END[i],
+                                       IN_START[i], IN_END[i])) %>%
+      mutate(FROM = as.character(data$FROM[i]),
+             TO = as.character(data$TO[i]))
+  }
+  
+  OUT = rbind_all(OUT) %>%
+    mutate(Chord = paste(FROM, TO, sep = ':'))
+}
--- dev/tidy_shapes2.R
+++ dev/tidy_shapes2.R
@@ -1,35 +1,42 @@
 #----
 library(dplyr)
-library(circly)
 library(ggplot2)
 remove(list = ls())
 
 #----
 source('./R/sample_data.R')
-source('./R/chord_spacing.R')
 data = sample_data(NROW = 5, NCOL = 5)
 
+# SHAPES = chord_shapes(data)
+
 #----
-source('./R/outer_spacing.R')
-segment = outer_spacing(data)
+source('./R/chord_spacing.R')
+segment_spaces = outer_spacing(data)
+chord_spaces = chord_spacing(data, segment_spaces)
+
 
 #----
 source('./R/outer_segment.R')
-shapes = outer_segments(segment)
+outer_shapes = outer_segments(segment_spaces)
+
+source('./R/chord_segment.R')
+chord_shapes = chord_segments(data)
 
 #----
 library(ggplot2)
-g = ggplot(segment) +
+g = ggplot(segment_spaces) +
   geom_segment(aes(cos(2 * pi * Start), sin(2 * pi * Start),
                    xend = cos(2 * pi * End), yend = sin(2 * pi * End))) +
   geom_path(aes(cos(seq(0, 2 * pi, length.out = 100)), sin(seq(0, 2 * pi, length.out = 100))),
             colour = 'royalblue') + 
-  coord_equal()
+  coord_equal() +
+  theme_minimal()
 print(g)
 
 #----
-g = ggplot(shapes) +
-  geom_polygon(aes(x, y, group = Label)) +
+g = ggplot(outer_shapes) +
+  geom_polygon(aes(x, y, group = Label, fill = Label)) +
+  geom_polygon(data = chord_shapes, aes(x, y, fill = Chord)) + 
   coord_equal() + 
   theme_minimal()
 print(g)
