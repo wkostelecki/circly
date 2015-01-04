34d152bd9898a06bfece2e4c9a7408894d49a4e5
Wojtek Kostelecki <wkostelecki@gmail.com>
2015-01-04 23:37:48 +0000

    Moved spacing function.

--- R/chord_spacing.R
+++ R/chord_spacing.R
@@ -20,3 +20,40 @@ respace = function(x, id, direction, gap_width = 0.005, ROI = 1){
   }
   x
 }
+
+
+
+
+outer_spacing = function(data, gap_width = 0.005, ROI = 1){
+  offset = 0.75
+  from_segment = data %>%
+    arrange(FROM, TO) %>%
+    rename(Label = FROM) %>%
+    group_by(Label) %>%
+    summarize(Size = sum(OUT)) %>%
+    mutate(Direction = 'Out',
+           nEnd = cumsum(Size) / sum(Size),
+           nStart = c(0, nEnd[1:(n() - 1)]),
+           compress = 1 - gap_width * n() / (1 / (ROI + 1)),
+           gaps = (gap_width / 2 + (gap_width * 0:(n() - 1))) / (1 / (ROI + 1)),
+           Start = (nStart * compress + gaps - 0.5) * (1 / (ROI + 1)) + offset,
+           End = (nEnd * compress + gaps - 0.5) * (1 / (ROI + 1)) + offset) %>%
+    select(Label, Direction, Start, End)
+  
+  offset = 0.25
+  to_segment = data %>%
+    arrange(FROM, TO) %>%
+    rename(Label = TO) %>%
+    group_by(Label) %>%
+    summarize(Size = sum(IN)) %>%
+    mutate(Direction = 'In',
+           nEnd = cumsum(Size) / sum(Size),
+           nStart = c(0, nEnd[1:(n() - 1)]),
+           compress = 1 - gap_width * n() / (ROI / (ROI + 1)),
+           gaps = (gap_width / 2 + (gap_width * 0:(n() - 1))) / (ROI / (ROI + 1)),
+           Start = -(nStart * compress + gaps - 0.5) * (ROI / (ROI + 1)) + offset,
+           End = -(nEnd * compress + gaps - 0.5) * (ROI / (ROI + 1)) + offset) %>%
+    select(Label, Direction, Start, End)
+  
+  rbind(from_segment, to_segment)
+}
