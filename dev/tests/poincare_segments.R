600b61edfa6a9ca2a3c5e5c3de70a92285bc2c76
Wojtek Kostelecki <wkostelecki@gmail.com>
2015-02-27 17:34:50 +0000

    made proper testing scripts

--- dev/tests/poincare_segments.R
+++ dev/tests/poincare_segments.R
@@ -1,17 +1,26 @@
 
 #  ------------------------------------------------------------------------
 
-v1 = 0.999
-x1 = cos(2 * pi * v1)
-y1 = sin(2 * pi * v1)
+context('poincare')
 
 
-v2 = 0.25
-x2 = cos(2 * pi * v2)
-y2 = sin(2 * pi * v2)
-
-
-data = circly:::poincare_segment(x1, y1, x2, y2)
-
-ggplot(as.data.frame(data)) + geom_line(aes(x,y)) + coord_equal() + scale_y_continuous(limits = c(-1, 1)) +
-  scale_x_continuous(limits = c(-1, 1))
+test_that('core function works', {
+  v1 = 0
+  x1 = cos(2 * pi * v1)
+  y1 = sin(2 * pi * v1)
+  
+  
+  v2 = 0.5
+  x2 = cos(2 * pi * v2)
+  y2 = sin(2 * pi * v2)
+  
+  
+  data = circly:::poincare_segment(x1, y1, x2, y2)
+  
+  expect_equal(data$x, c(1, -1))
+  expect_equal(data$y, c(0, 0))
+  
+  
+})
+# ggplot(as.data.frame(data)) + geom_line(aes(x,y)) + coord_equal() + scale_y_continuous(limits = c(-1, 1)) +
+#   scale_x_continuous(limits = c(-1, 1))
